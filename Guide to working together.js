d6f21fec96cb452b650516360c16f4a953591250
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2018-01-16 19:00:21 +0800

    Guide to working together
    Every one should read this first before they start.

---
+++ Guide to working together.js
@@ -0,0 +1,144 @@
+const actOne = {
+  act: {
+    title: 'Act I: Project Setup',
+    order_index: 0,
+    repeatable: false,
+    milestones: [
+      {
+        title: 'Meet the Team',
+        order_index: 0,
+        description: `
+*Goal*: Get acquainted with your teammates.
+
+*Suggested Actions*: 
+> Read each other''s intro stories.
+> Share a project or two you are most proud of in your learning journey.
+> Share some prime picks from your gif collection.
+
+*Icebreakers*:
+> What time zone are you in?
+> What sort of music do you like to code to? 
+> What is your preferred learning style (videos, docs, dialoge etc)?
+> What are your favorite YouTube channels, subreddits or blogs?
+> What technologies are you most comfortable using and explaining?
+> What technologies are you most interested in learning this Voyage?
+        `,
+        resource_url: null,
+      },
+      {
+        title: 'Set Goals and Expectations',
+        order_index: 1,
+        description: `
+*Goal*: Determine the team''s learning expectations and availability.
+
+*Commitment*: 
+> It is critical to make sure that everyone is realistic in their level of commitment and availability.
+> Working less than you commit to is disrespectful to your teammates and can stunt the team''s progress and morale.
+> Typically team members commit 4-10 hours a week towards the project.
+
+*Learning Goals*: 
+> Evaluate everyone''s current technical abilities.
+> Determine at least one primary technical learning goal for each teammate
+> Pick something that is 25% outside of the team''s comfort zone / skillset. 
+> Any further and you will likely be overwhelmed. 
+> Any less and you are limiting your rate of learning. 
+
+*Voyages go by faster than you think and you need to focus on learning through building rather than getting caught in research rabbit-holes*.
+        `,
+        resource_url: null,
+      },
+      {
+        title: 'Brainstorm Project Options',
+        order_index: 2,
+        description: `
+*Goal*: Decide on a project to pursue.
+
+*Tips*:
+> Stay focused on what is possible as a balance of the cohort season''s length, team experience and the commitment level.
+> Remember to incorporate as many team learning goals as are feasible. 
+> *The goal is to learn something new not to stay stuck only working with what you know.*
+
+*The Stack*:
+> Front End - consider the library / framework your project will use
+> Back End - consider the backend language and library / framework
+> Database - consider the database type and ORM your team will use to interact with it
+
+*It is better to underpromise and overdeliver _especially if this is your first Voyage_*
+        `,
+        resource_url: null,
+      },
+      {
+        title: 'Define Your Workflow',
+        order_index: 3,
+        description: `
+*Goal*: Decide on which workflow tools the team will use.
+
+*Tips*:
+> Don''t go overboard! Workflow tools are only useful when they are used. Keep it simple so that using the tools feels effortless rather than a chore.
+> If you don''t already have a workflow or specific tools in mind consider the suggestions below.
+
+*Suggested Workflow Tools*
+>Besides the obvious Slack and Github the following are tools that work for many Chingu teams
+> <https://www.waffle.io|Waffle> - Trello and Github had a baby that makes project management a breeze
+> <https://www.discord.com|Discord> - "Always-on" communication tool. Leave it on mute while you work then unmute as needed to gain access to team mates for problem solving detours. Use it for clear audio while using Google Hangouts (don''t forget to mute the echo from Hangouts!)
+> <https://hangouts.google.com|Google Hangouts> - A screensharing / pair-programming must (until VSC Live comes out!!)
+
+*Suggested Use*: 
+> Hold a meeting on Discord and / or Hangouts to discuss the key steps required to complete the next sprint.
+> Use Waffle to compartmentalize the steps of the sprints into grab-and-go tasks.
+> Leave Discord on and unmute as needed to discuss and conquer roadblocks.
+> Use Slack for short code snippets. Use Hangouts to screenshare for more complex debugging.
+
+*A proper workflow and concise collection of workflow tools is what makes the difference between a team that makes it and one that doesn''t.*
+        `,
+        resource_url: null,
+      },
+      {
+        title: 'Set Up',
+        order_index: 4,
+        description: `
+*Goal*: Set up and explore your team''s workflow tools.
+
+*Tips*:
+> Check to see if your pre-made Voyage team repo is functional and the proper permissions and settings are configured.
+> Ask for help from the community with setting up tools if you get stuck. 
+> Don''t get turned off because the setup seems daunting. It''s a one time process - take the time to do it right!
+
+*Required*:
+> Make sure everyone uses the tools. 
+> Walk through an example usage of each tool and how they all come together.
+> Work out any kinks in people''s microphones, internet connections, permissions etc.
+
+*The first sprint IS NOT the time to learn how to use the tools at hand.* 
+*Get comfortable early so you can focus on building not technical difficulties!* 
+        `,
+        resource_url: null,
+      },
+      {
+        title: 'Define the MVP',
+        order_index: 5,
+        description: `
+*Goal*: Hold a meeting to decide on the minimum viable product (MVP).
+
+*Tips*:
+> Deciding on the MVP for the project is the most important step your team must take before embarking on your Voyage.
+> Consider the features that can together serve, at minimum, as a functioning prototype of the project's vision.
+> Defining the MVP is committing to the bare minimum you and the team will accomplish before the cohort deadline.
+
+*Focus Points*:
+> Front End - consider the views and their associated functionality
+> Back End - consider the endpoints, routes and associated logic
+> Database - consider the complexity of the database and associated models
+> Deployment - consider how / where you will host the project
+
+*Schedule a Meeting*
+> Schedule your next meeting to plan your first sprint!
+
+*Don't throw away those features and moonshot ideas that fall outside of the scope of the MVP!*
+*Shoot for finishing the MVP ahead of schedule so the extra time can be used to implement those additional features and polish* 
+        `,
+        resource_url: null,
+      },
+    ],
+  },
+};
